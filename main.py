5f87a56587476ac9176a79584a65f6aa72edd2b5
wusara87 <wusara87@csu.fullerton.edu>
2024-02-28 19:03:32 -0800

    updated login page

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-#import kivy modules
 from xmlrpc.client import SafeTransport
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
+from kivy.uix.label import MarkupLabel
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 #load the kv file
 Builder.load_file('login.kv')
@@ -13,7 +14,7 @@ Builder.load_file('login.kv')
 class LoginLayout(BoxLayout):
     pass
 
-class CantoConnect(App):
+class CantoConnect(Screen):
     def build(self):
         return LoginLayout()
     
@@ -22,11 +23,14 @@ class CantoConnect(App):
         username = self.root.ids.username_input.text
         password = self.root.ids.password_input.text
 
-        if username == 'user' and password == 'pass':
+        if username == 'user@gmail.com' and password == 'pass':
             print('Login successful.')
         else:
             print('Login failed.')
         
+class CreateAccount(App):
+    pass
+
 
 if __name__ == '__main__':
     CantoConnect().run()
